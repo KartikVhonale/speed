ac48483dff965e8ed61d070f54ed19660cb4639a
kartik <whonalekartik11@gmail.com>
2022-12-30 10:55:14 +0530

    initial commite

---
+++ src/App.svelte
@@ -0,0 +1,55 @@
+<script>
+
+</script>
+
+<main>
+  <nav class="navbar navbar-expand-lg text-light navbar-dark" data-bs-theme="dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img src="/wifi_logo.png" class="logo"/></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end"><ul class="nav ">
+        <li class="nav-item">
+          <a href="#" class="nav-link">Home</a>
+        </li>
+        <li>
+          <a href="#" class="nav-link">About</a>
+        </li>
+      </ul></div>
+      
+    </div>
+  </nav>
+    
+</main>
+
+<style>
+
+
+  ul {
+    margin: 14px 3px;
+    padding: 3px 17px;
+    color: aliceblue;
+    size: 44pt;
+  }
+  a {
+    color: white
+  }
+  li a{
+    border-radius: 5px;
+  }
+  li a:hover{
+    background-color: orange;
+    color: black; 
+    border-radius: 5px;
+
+  }
+  nav {
+    background-color: #060c59;
+  }
+
+  .logo {
+    height: 80px;
+  }
+
+</style>
