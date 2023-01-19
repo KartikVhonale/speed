3737bbda64eecb74307fd36e7949291baffcab83
Kartik Mahadev Vhonale <98228313+KartikVhonale@users.noreply.github.com>
2023-01-19 20:32:45 +0530

    Add files via upload

--- src/App.svelte
+++ src/App.svelte
@@ -1,55 +1,14 @@
-<script>
 
+
+<script>
+import Header from './components/Header.svelte';
+import Body from './components/Body.svelte';
 </script>
 
 <main>
-  <nav class="navbar navbar-expand-lg text-light navbar-dark" data-bs-theme="dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#"><img src="/wifi_logo.png" class="logo"/></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse justify-content-end"><ul class="nav ">
-        <li class="nav-item">
-          <a href="#" class="nav-link">Home</a>
-        </li>
-        <li>
-          <a href="#" class="nav-link">About</a>
-        </li>
-      </ul></div>
-      
-    </div>
-  </nav>
-    
+  <Header />
+  <Body /> 
 </main>
 
 <style>
-
-
-  ul {
-    margin: 14px 3px;
-    padding: 3px 17px;
-    color: aliceblue;
-    size: 44pt;
-  }
-  a {
-    color: white
-  }
-  li a{
-    border-radius: 5px;
-  }
-  li a:hover{
-    background-color: orange;
-    color: black; 
-    border-radius: 5px;
-
-  }
-  nav {
-    background-color: #060c59;
-  }
-
-  .logo {
-    height: 80px;
-  }
-
 </style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,55 @@
+<main>
+  <nav class="navbar navbar-expand-lg text-light navbar-dark" data-bs-theme="dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img src="/logo.png" class="logo"/></a>
+      <button class="navbar-toggler dropdown" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+        <li class="nav-item">
+            <a href="#" class="nav-link ">Home</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link ">About Us</a>
+        </li>
+        <li>
+          <a href="#" class="nav-link ">Help</a>
+        </li>
+      </ul>
+    </div>
+    </div>
+  </nav>
+  </main>
+
+  <style>
+  img{
+    background-color: white;
+  }
+  ul {
+    margin: 14px 3px;
+    padding: 3px 17px;
+    color: aliceblue;
+    size: 44pt;
+  }
+  a {
+    color: white;
+  }
+  li a{
+    border-radius: 5px;
+  }
+  li a:hover{
+    background-color: orange;
+    color: black; 
+    border-radius: 5px;
+
+  }
+  nav {
+    background-color: #060c59;
+  }
+
+  .logo {
+    height: 3rem;
+    width: 7rem;
+  }
+  </style>
